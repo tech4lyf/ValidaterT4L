bb7e5e3e90aa6219b421a31c72da843661e11f3d
#Tech4LYF <tech4lyf2017@gmail.com>
2020-12-18 03:01:33 +0530

    Initial commit

---
+++ app/src/main/java/com/tech4lyf/validatert4l/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tech4lyf.validatert4l;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
